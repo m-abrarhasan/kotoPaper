bc1f6c6d800adf47f6600adaa2f24e1b1d55d94b
Abrar <61474167+m-abrarhasan@users.noreply.github.com>
2021-10-19 17:23:33 +0600

    first few lines

--- main.c
+++ main.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main()
+{
+  double aFour = (8.3*11.7);
+  double demy = (18*23);
+  double sheetsReq = (18*23);
+  
+  printf("The size of a A4 paper is >> %f\n",aFour);
+
+
+
+
+  return 0;
+}
