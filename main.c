a37a37f6d5672f3f31a7d42e4f2444d9ec74064c
abrar <m-abrarhasan@outlook.com>
2021-10-21 15:26:37 +0600

    first commit

--- main.c
+++ main.c
@@ -2,14 +2,7 @@
 
 int main()
 {
-  double aFour = (8.3*11.7);
-  double demy = (18*23);
-  double sheetsReq = (18*23);
-  
-  printf("The size of a A4 paper is >> %f\n",aFour);
 
-
-
-
+printf("somethin somethin")
   return 0;
 }
