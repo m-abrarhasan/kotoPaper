e6dcf82111bff0dd31850d14d8168329fb0806fa
m-abrarhasan <61474167+m-abrarhasan@users.noreply.github>
2021-12-16 23:12:08 +0600

    update tui, add paper class, add headers and classes folder

---
+++ headers/func.h
@@ -0,0 +1,29 @@
+// clear
+void clear() {
+
+	//system("cls");
+	system("clear");
+}
+
+// tui
+void tui(std::string txtpath) {
+
+	std::ifstream getui(txtpath);
+
+	if (getui.is_open()) {
+		std::cout << getui.rdbuf();
+	}
+}
+
+// user input
+int get_option(std::string txtpath) {
+
+	clear(); //clear screen
+
+	int PrintSize;
+	tui(txtpath);
+	std::cin >> PrintSize;
+
+	clear(); //clear screen
+	return PrintSize;
+}
