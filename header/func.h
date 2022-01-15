5ce9e3337f196b50f9322674e025722413a8553e
m-abrarhasan <61474167+m-abrarhasan@users.noreply.github>
2022-01-15 21:41:25 +0600

    just a reminder for future

--- header/func.h
+++ header/func.h
@@ -32,11 +32,11 @@ label:
   std::cin >> _in;
   if (_in < 1 || _in > 5) {
     if (_in == 6) {
-      std::cout << "goto constructor for paper area" << std::endl;
-      getchar();
-      getchar();
-    } else {
+      std::cout << "\nERROR: Custom Not Available Yet!!" << std::endl;
       std::cout << "Wrong Input! Please Enter Again  > ";
+      goto label;
+    } else {
+      std::cout << "\nWrong Input! Please Enter Again  > ";
       goto label;
     }
   }
