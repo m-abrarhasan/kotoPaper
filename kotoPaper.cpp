5665e6ef94a1c453cda6678218d209c266f82c1d
m-abrarhasan <61474167+m-abrarhasan@users.noreply.github.com>
2022-05-22 15:34:10 +0600

    reworking [ongoing -2 ]

--- kotoPaper.cpp
+++ kotoPaper.cpp
@@ -12,41 +12,33 @@ int main()
   paper half_demy(18.00, 23.00, "Half Demy\t[ 18x23 ]");
 
   // print_sizes
-  paper one_4(09.00, 11.50, "One Four\t[ 9x11.5 ]");
-  paper one_8(5.75, 09.00, "One Eight\t[ 5.75x9.00 ]");
+  paper one_4(09.00, 11.50, "One Four\t[ 9.00 x 11.5 ]");
+  paper one_8(5.75, 09.00, "One Eight\t[ 5.75 x 9.00 ]");
 
-  vector<paper> print_size = {one_4, one_8};
-  vector<paper> media_size = {double_demy, half_demy};
+  vector<paper> print_sizes = {one_4, one_8};
+  vector<paper> media_sizes = {double_demy, half_demy};
 
-  vector<string> pSizeName = {one_4.name, one_8.name};
-  vector<string> mSizeName = {double_demy.name, half_demy.name};
+  // do
+  // {
 
+  // user input
   char border = '=';
+  clear();
+  const int op_print = getOption(print_sizes, border, "Print Size");
+  // clear();
+  // const int op_media = getOption(media_sizes, border, "Sheet Size");
 
-  do
-  {
+  // const int paper_rate = getui_num("tui/paper_rate.txt"); // Per Ream
+  // const int order_size = getui_num("tui/order_size.txt");
 
-    // user input
-    const int pSizePos = getOption(pSizeName, border);
+  // calculation
+  // double total_sheet = get_sheet(job_size, sheet_size, order_size);
+  // double total_cost = get_cost(total_sheet, paper_rate);
 
-    // std::cout << "\n"
-    //           << one_4.name.size();
-    // std::cout << "\n"
-    //           << one_8.name.size();
+  // output
+  // putui(total_sheet, total_cost, "tui/total.txt");
 
-    // const int media_size = tui(media_obj);
-
-    // const int paper_rate = getui_num("tui/paper_rate.txt"); // Per Ream
-    // const int order_size = getui_num("tui/order_size.txt");
-
-    // calculation
-    // double total_sheet = get_sheet(job_size, sheet_size, order_size);
-    // double total_cost = get_cost(total_sheet, paper_rate);
-
-    // output
-    // putui(total_sheet, total_cost, "tui/total.txt");
-
-  } while (get_enter());
+  // } while (get_enter("press enter to continue..... "));
 
   return 0;
 }
--- kotolib/input.hpp
+++ kotolib/input.hpp
@@ -8,12 +8,23 @@
 
 using namespace std;
 
-int getOption(vector<string> nameList, char &border)
+int getOption(vector<paper> list, char &border, string header1)
 {
-    int vPad = {1 * 2};
-    int hPad = {2 * 2};
+    // get vector of name from list
+    vector<string> nameList;
+    // need to learn how transform works
+    transform(
+        list.begin(), list.end(), std::back_inserter(nameList),
+        [](const paper &v)
+        { return v.name; });
 
-    int rows = 7 + vPad + nameList.size();
+    // build header-1 & header-2
+    string h1 = ":: ==> Select " + header1;
+    string h2 = ":: ==> Select Option   [0-" + to_string((nameList.size() - 1)) + "]";
+    int vPad = {1};
+    int hPad = {6};
+
+    int rows = 6 + (vPad * 2) + nameList.size();
 
     // this probably uses lambda, idk yet ;)
     auto result = std::max_element(nameList.begin(), nameList.end(),
@@ -22,19 +33,28 @@ int getOption(vector<string> nameList, char &border)
                                        return a.size() < b.size();
                                    });
     int index = distance(nameList.begin(), result);
-    string::size_type cols = 1 + nameList[index].size() + hPad;
+
+    // get the size of largest string to calculate cols
+    auto Lstring = max({nameList[index].size(), h1.size(), h2.size()});
 
     // invariant: written r number of rows so far
     for (int r = 0; r != rows; ++r)
     {
+        string::size_type cols = 1 + Lstring + (hPad * 2);
+
         // invariant: written c number cols so far
         string::size_type c = 0;
         while (c != cols)
         {
-            if (r == 0 || r == 2 || r == rows - 4 || r == rows - 2 || (c == cols - 1 && r != rows - 1))
+            if (r == 0 || r == 2 || r == rows - 3 || r == rows - 1 || c == cols - 1)
             {
                 std::cout << border;
                 ++c;
+            }
+            else if (r == vPad + 3 && c == hPad)
+            {
+                cout << "hello";
+                c += 5;
             }
             else
             {
@@ -42,14 +62,20 @@ int getOption(vector<string> nameList, char &border)
                 ++c;
             }
         }
-        if (rows != (rows - 1))
-        {
-            std::cout << "\n";
-        }
+        std::cout << "\n";
+    }
+
+    for (int i = 0; i < list.size(); i++)
+    {
+        std::cout << i
+                  << " -> "
+                  << list[i].name
+                  << "\n";
     }
 
     int option = {0};
-    cin >> option;
+    cout << "> ";
+    // cin >> option;
     // validate option
 
     return option;
