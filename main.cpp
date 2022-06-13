3efffc24d7788c5629ba6e0b42695e90e87bbd1b
m-abrarhasan <61474167+m-abrarhasan@users.noreply.github.com>
2022-06-13 19:47:03 +0600

    set QtStyle to fusion

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QStyleFactory>
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    a.setStyle(QStyleFactory::create("Fusion"));
     w.show();
     return a.exec();
 }
