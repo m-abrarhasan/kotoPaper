71b1080218b97631456d9fe6fcb139cf18b1d44e
m-abrarhasan <61474167+m-abrarhasan@users.noreply.github.com>
2022-06-14 22:13:47 +0600

    minor changes

--- media.hpp
+++ media.hpp
@@ -2,7 +2,9 @@
 #include "paper.h"
 #include <vector>
 // media_sizes
-Paper double_demy(23.00, 36.00, "Double Demy\t[ 23x36 ]");
-Paper half_demy(18.00, 23.00, "Half Demy\t[ 18x23 ]");
+Paper double_demy ( 23.00, 36.00, "Double Demy [ 23x36 ]" );
+Paper half_demy ( 18.00, 23.00,   "Half Demy   [ 18x23 ]" );
 
-std::vector<Paper> medias = { double_demy, half_demy };
+std::vector<Paper> medias = { double_demy,
+                              half_demy
+                            };
--- prints.hpp
+++ prints.hpp
@@ -3,7 +3,10 @@
 #include <vector>
 
 // print_sizes
-Paper one_4(09.00, 11.50, "One Four\t[ 9.00 x 11.5 ]");
-Paper one_8(5.75, 09.00, "One Eight\t[ 5.75 x 9.00 ]");
+Paper one_4 ( 09.00, 11.50, "One Four   [ 9.00 x 11.5 ]" );
+Paper one_8 ( 5.75, 09.00,  "One Eight  [ 5.75 x 9.00 ]" );
 
-std::vector<Paper> prints = { one_4, one_8 };
+std::vector<Paper> prints = { one_4,
+                              one_8
+                            };
+// kate: indent-mode cstyle; indent-width 5; replace-tabs on; 
