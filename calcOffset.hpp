2b8673f4686e80f27b31cd39afad481630e10149
Abrar <61474167+m-abrarhasan@users.noreply.github.com>
2023-03-12 19:03:29 +0600

    make calcOffset inline

--- calcOffset.hpp
+++ calcOffset.hpp
@@ -5,26 +5,22 @@
 #include <QDebug>
 #include <QPalette>
 
-void
-calcOffset(Ui::MainWindow* ui)
+inline void calcOffset(Ui::MainWindow* ui)
 {
+    double mediaRate = ui->spnbox_MediaRate->value();
+    double orderSize = ui->spnbox_OrderSize->value();
 
-  double mediaRate = ui->spnbox_MediaRate->value();
-  double orderSize = ui->spnbox_OrderSize->value();
+    double mArea = mediaStore[ui->cmbox_Media->currentIndex()].getArea();
+    double pArea = printStore[ui->cmbox_PrintSize->currentIndex()].getArea();
 
-  double mArea = mediaStore[ui->cmbox_Media->currentIndex()].getArea();
-  double pArea = printStore[ui->cmbox_PrintSize->currentIndex()].getArea();
+    double totalSheet = orderSize / (mArea / pArea);
+    double totalCost  = totalSheet * (mediaRate / 500);
 
-  double totalSheet = orderSize / (mArea / pArea);
-  double totalCost = totalSheet * (mediaRate / 500);
+    ui->lbl_TotalSheet->setText(QString::number(totalSheet));
+    ui->lbl_TotalSheet->setStyleSheet("QLabel {font-size : 11pt; font-weight : 700; color:#00aa00;}");
+    qDebug() << " :: Total Sheet" << totalSheet << "\n";
 
-  ui->lbl_TotalSheet->setText(QString::number(totalSheet));
-  ui->lbl_TotalSheet->setStyleSheet(
-    "QLabel {font-size : 11pt; font-weight : 700; color:#00aa00;}");
-  qDebug() << " :: Total Sheet" << totalSheet << "\n";
-
-  ui->lbl_TotalCost->setText(QString::number(totalCost));
-  ui->lbl_TotalCost->setStyleSheet(
-    "QLabel {font-size : 11pt; font-weight : 700; color:#00aa00;}");
-  qDebug() << " :: Total Cost" << totalCost << "\n";
+    ui->lbl_TotalCost->setText(QString::number(totalCost));
+    ui->lbl_TotalCost->setStyleSheet("QLabel {font-size : 11pt; font-weight : 700; color:#00aa00;}");
+    qDebug() << " :: Total Cost" << totalCost << "\n";
 }
